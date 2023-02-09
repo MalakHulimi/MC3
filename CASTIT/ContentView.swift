e23dc81697f38ecbd4585687e95902a0b48d883a
Dai AL-Tamimi <116816097+DaiTamimi@users.noreply.github.com>
2023-02-09 17:25:20 +0300

    v

--- CASTIT/ContentView.swift
+++ CASTIT/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "cloud")
+            Image(systemName: "Meow")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("updated !")
