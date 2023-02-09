c813ec705fcf3cdd0f529e8280ff763b182a5ece
MalakHulimi <malakhulimi@gmail.com>
2023-02-09 16:05:59 +0300

    Initial Commit

---
+++ CASTIT/CASTITApp.swift
@@ -0,0 +1,17 @@
+//
+//  CASTITApp.swift
+//  CASTIT
+//
+//  Created by Malak  on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CASTITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
