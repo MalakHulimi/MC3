3f2effe9dbf298896d7bcf878b6178ddbfab0974
malak <malakhulimi@gmail.com>
2023-02-09 17:25:02 +0300

    add lamya

--- CASTIT/ContentView.swift
+++ CASTIT/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "cloud")
+            Image(systemName: "lamya")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("updated !")
