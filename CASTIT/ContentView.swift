db5ab7bc6f7e685ed32bb45218a478f9ce5c7901
MalakHulimi <malakhulimi@gmail.com>
2023-02-09 16:23:31 +0300

    update 2

--- CASTIT/ContentView.swift
+++ CASTIT/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "person")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("updated !")
         }
         .padding()
     }
